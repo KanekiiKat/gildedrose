e700f81c4c83240cecec0d90a70e3c6d268bdc5c
KanekiiCat <alex123viri@gmail.com>
2025-05-27 17:38:22 +0200

    feat(GildedRose): Añadir metodos

--- src/main/java/edu/estatuas/gildedrose/GildedRose.java
+++ src/main/java/edu/estatuas/gildedrose/GildedRose.java
@@ -1,5 +1,29 @@
 package edu.estatuas.gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.estatuas.gildedrose.items.Updateable;
+
+
 public class GildedRose {
 
+    private List<Updateable> inventory = new ArrayList<Updateable>();
+
+    public GildedRose() {
+    }
+
+    public List<Updateable> inventory(){
+        return inventory;
+    }
+
+    public void addItem(Updateable item){
+        inventory.add(item);
+    }
+
+    @Override
+    public String toString(){
+        return inventory.toString();
+    }
+    
 }
