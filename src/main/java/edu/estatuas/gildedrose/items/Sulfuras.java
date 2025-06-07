4953521e990b57356fbee1ca8e5f9aefb05170c3
KanekiiCat <alex123viri@gmail.com>
2025-06-07 12:55:43 +0200

    fix(Sulfuras): Arreglar constructor

--- src/main/java/edu/estatuas/gildedrose/items/Sulfuras.java
+++ src/main/java/edu/estatuas/gildedrose/items/Sulfuras.java
@@ -3,7 +3,7 @@ package edu.estatuas.gildedrose.items;
 public class Sulfuras extends NormalItem{
     
 
-    public Sulfuras(String name, int quality, int sell_in) {
+    public Sulfuras(String name, int sell_in, int quality) {
         super(name, sell_in, quality);
     }
 
